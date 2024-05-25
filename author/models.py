5c5d5cfb82ecbd9146b4f8990be41403519f6272
kekW <panicua12@gmail.com>
2024-05-25 18:27:19 +0300

    feat: add Author(modelserializer) to serializers, add Author(modelveiwset) to views

--- author/models.py
+++ author/models.py
@@ -5,5 +5,5 @@ class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     pseudonym = models.CharField(max_length=64, null=True, blank=True)
-    age = models.IntegerField()
+    age = models.PositiveIntegerField()
     retired = models.BooleanField()
--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    id = serializers.IntegerField(read_only=True)
+    pseudonym = serializers.CharField(required=False, max_length=64)
+
+    class Meta:
+        model = Author
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "pseudonym",
+            "age",
+            "retired",
+        ]
