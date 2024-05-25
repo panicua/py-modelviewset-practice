d9415c9ed07618e1acece98907fffcd6c2df1c1d
kekW <panicua12@gmail.com>
2024-05-25 18:29:49 +0300

    removed id attribute from AuthorSerializer(built-in)

--- author/serializers.py
+++ author/serializers.py
@@ -4,7 +4,6 @@ from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
     pseudonym = serializers.CharField(required=False, max_length=64)
 
     class Meta:
